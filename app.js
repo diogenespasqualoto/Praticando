0121ae05897a69bbb29dbfe73fdbe155f17014c7
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-12-06 16:07:47 -0300

    script JS finish

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-'use strict';
+var submitButton = document.querySelector('#app form button');
+var zipCodeField = document.querySelector('#app form input');
+var content = document.querySelector('#app main');
 
-var submitButton = document.querySelector('#app form button')
-var zipCodeField = document.querySelector('#app form input')
-var content = document.querySelector('#app main')
-
-submitButton.addEventListener('click', run)
-
+//2º Criando a função para quando clicarmos no botão de consulta:
 function run(event) {
-    event.preventDefault()
+    //Não deixa a página atualizar quando clicamos no botão consulta
+    event.preventDefault();
+    //Dá o valor para a variável zipCode, o que o usuário digitou no zipCodeField
+    var zipCode = zipCodeField.value;
+    //Remove um possível espaço entre os números
+    zipCode = zipCode.replace(' ', '');
+    //Remove um possível . entre os números
+    zipCode = zipCode.replace('.', '');
+    //Remove um possível - entre os números
+    zipCode = zipCode.replace('-', '');
+    //Remove um espaçamento no final ou início do número
+    zipCode = zipCode.trim();
+    //Mostra o CEP digitado
+    console.log(zipCode);
 
-    var zipCode = zipCodeField.value
+    //Mostra os dados do CEP abaixo do botão de consulta
+    function createLine(text) {
+        var line = document.createElement('p');
+        var text = document.createTextNode(text);
 
-    zipCode = zipCode.replace(' ', '')
-    zipCode = zipCode.replace('.', '')
-    zipCode = zipCode.trim()
+        line.appendChild(text);
+        content.appendChild(line);
+    }
 
-    console.log(zipCode)
-
-    axios.get('https://viacep.com.br/ws/' + zipCode + '/json/')
+    //Faz a consulta com o server utilizando a axios
+    axios
+        .get('https://viacep.com.br/ws/' + zipCode + '/json/')
         .then(function (response) {
-            console.log(response.data)
+            //Se digita um CEP inexistente
+            if (response.data.erro) {
+                createLine('Opa, esse teu CEP não existe! ');
+            }
+            content.innerHTML = '';
+            //Usa a função de criar linhas pra inserir os dados do CEP 
+            createLine(response.data.localidade + '/' + response.data.uf);
+            createLine(response.data.logradouro);
+            createLine('Bairro: ' + response.data.bairro);
         })
         .catch(function (error) {
-            console.log(error)
-        })
+            console.log(error);
+            //Apaga os dados da consulta anterior
+            content.innerHTML = '';
+            createLine('Ops, algo deu errado! 🥺');
+        });
 }
 
-function createLine(text) {
-    var line = document.createElement('p')
-    var text = document.createTextNode(text)
-
-    line.appendChild(text)
-    content.appendChild(line)
-}
+//3º Executa a função quando clicamos no botão de consulta:
+submitButton.addEventListener('click', run);
